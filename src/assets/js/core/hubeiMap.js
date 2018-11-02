a46ea53d2ee0f8bcef3812d85525b647c3dcd181
admin <HWB@gankki>
2018-11-02 12:54:26 +0800

    地图 zoom调整“

--- src/assets/js/core/hubeiMap.js
+++ src/assets/js/core/hubeiMap.js
@@ -414,7 +414,7 @@ function timeTicket() {
 			regions: customSettings,
 			// 地图控制 是否缩放+拖拽 move 和 scale
 			roam: false,
-			zoom:1.1,
+			zoom:1.2,
 			itemStyle: {
                 normal: {
                     // borderColor: 'rgba(147, 235, 248, 1)',
